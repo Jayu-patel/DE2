37e2afc9b9d1f723da0dd841ecf8815e10612bd5
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2024-04-28 16:43:31 +0530

    Update SearchPosts.jsx

--- src/components/hospital/SearchPosts.jsx
+++ src/components/hospital/SearchPosts.jsx
@@ -64,7 +64,7 @@ function SearchPosts({posts}) {
         {
             posts?.map((e)=>{
                 return( 
-                  e?.bed < 0 ? <></> :
+                  e?.bed <= 0 ? <></> :
                   <div key={e?.id} className='py-1'>
                           <div className='flex justify-between w-[85%] xs:w-full mx-auto p-3 border-[1px] border-black' style={{backgroundImage : "url('bg.jpg')"}}>
                               <div className='ml-10 xs:ml-0'>
